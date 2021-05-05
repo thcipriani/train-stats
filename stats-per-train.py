94472d3a3a883da6f1411427fb78146000ecc3d7
Tyler Cipriani <tcipriani@wikimedia.org>
2021-05-04 18:01:56 -0600

    Add moar stats

--- stats-per-train.py
+++ stats-per-train.py
@@ -14,6 +14,8 @@ from bs4 import BeautifulSoup
 
 from distutils.version import LooseVersion
 from dateutil import parser
+from datetime import date, datetime
+from statistics import mode
 
 BASE_URL = 'https://www.mediawiki.org/wiki/'
 FMT_URL = 'MediaWiki_{}/Changelog'
@@ -112,13 +114,21 @@ def set_version(version_diff, wikiversions_line):
     return version_diff
 
 
-def is_rollback(wikiversion, change):
-    wikiversion = LooseVersion(wikiversion)
+def wikiversion_info(version, change):
+    wikiversion = LooseVersion(version)
     wikis = {}
     # Get the diff for the change
     diff = subprocess.check_output([
         'git', '-C', MWCONFIG_PATH, 'diff-tree', '-p', '-U0', change, 'wikiversions.json'
     ])
+
+    committer = subprocess.check_output([
+        'git', '-C', MWCONFIG_PATH, 'log', '-1', '--format=%cN', change
+    ]).decode('utf8').strip()
+
+    commit_date = int(subprocess.check_output([
+        'git', '-C', MWCONFIG_PATH, 'log', '-1', '--format=%ct', change
+    ]).decode('utf8').strip())
 
     # That's right. It's a regex for parsing a diff. Fight me.
     for line in diff.splitlines():
@@ -142,32 +152,97 @@ def is_rollback(wikiversion, change):
         wikis[wiki] = set_version(version_diff, line)
 
     rollback = False
-    for wiki, version in wikis.items():
+    rollforward = False
+    for w, v in wikis.items():
         try:
             # If the old version of wikiversions.json had the wikiversion we're investigating
             # AND
             # the new wikiversions.json has an OLDER wikiversion than the one we're investigating           #
-            if wikiversion == version.old_version and version.new_version < wikiversion:
+            if wikiversion == v.old_version and v.new_version < wikiversion:
                 rollback = True
+            # TODO: this could be wrong if we roll back and forward for different wikis in the same commit
+            if wikiversion == v.new_version and v.old_version < wikiversion:
+                rollforward = True
         except TypeError:
             # This happens when you add or remove a wiki -- there won't be an
             # old_version or a new_version, respectively
             pass
 
-    return rollback
+    return {
+        'rollback': rollback,
+        'rollforward': rollforward,
+        'wikis': wikis,
+        'committer': committer,
+        'commit_date': commit_date,
+        'version': version,
+        'sha1': change.decode('utf8'),
+    }
+
+
+def get_conductor(version, changes):
+    """
+    if it's not a rollback and the new version is the version we're looking for
+    the author is probably the conductor
+    """
+    conductors = []
+    for change in changes:
+        if change['rollforward']:
+            # debug
+            print('{}: {} ROLLFORWARD'.format(change['committer'], change['sha1']))
+            conductors.append(change['committer'])
+    return mode(conductors)
+
+
+def time_rolledback(version, changes):
+    total_seconds = 0
+    start = None
+    for change in sorted(changes, key=lambda x: x['commit_date']):
+        if change['rollback'] and start is None:
+            start = change['commit_date']
+            continue
+        if start is not None and change['rollforward']:
+            total_seconds += change['commit_date'] - start
+            start = None
+
+    return total_seconds
 
 
 def count_rollbacks(version, changes):
     rollbacks = 0
     for change in changes:
-        print('{}: {} MENTION'.format(version, change.decode('utf8')))
-        if is_rollback(version, change):
+        print('{}: {} MENTION'.format(version, change['sha1']))
+        if change['rollback']:
             # debug
-            print('{}: {} ROLLBACK'.format(version, change.decode('utf8')))
+            print('{}: {} ROLLBACK'.format(version, change['sha1']))
             rollbacks += 1
     return rollbacks
 
-def get_rollbacks_for_version(version):
+def total_train_time(version, changes):
+    sorted_commits = sorted(changes, key=lambda x: x['commit_date'])
+    return sorted_commits[-1]['commit_date'] - sorted_commits[0]['commit_date']
+
+def train_delays(version, changes):
+    # Monday = 1; Sunday = 7
+    expected = {
+        'mediawiki': 2,   # Tuesday
+        'commons': 3,     # Wednesday
+        'enwiki': 4,      # Thursday
+    }
+    # Initialized to preserve the order for return
+    delays = { 'mediawiki': 0, 'commons': 0, 'enwiki': 0 }
+    for change in sorted(changes, key=lambda x: x['commit_date']):
+        if change['rollforward']:
+            for wiki, exp in expected.items():
+                if change['wikis'].get(wiki, VersionDiff()).new_version == version:
+                    # Modulo 7 to account for trains that finish the following week
+                    delays[wiki] = (
+                        date.fromtimestamp(change['commit_date']).isoweekday() - exp
+                    ) % 7
+
+    return delays.values()
+
+
+def get_wikiversion_changes(version):
     # Oldest commit to wikiversions mentioning "version"
     oldest_cmd = [
         '/usr/bin/git',
@@ -193,15 +268,10 @@ def get_rollbacks_for_version(version):
         'wikiversions.json'
     ]
     all_changes_with_version = subprocess.check_output(oldest_cmd).splitlines()
-    return count_rollbacks(version, all_changes_with_version)
-
-
-def get_version_info(version):
-    rollbacks = get_rollbacks_for_version(version)
-    if rollbacks is None:
-        raise RuntimeError(f'{version} was never deployed!')
-    patches, version = get_patches_for_version(version)
-    return (patches, rollbacks, version)
+    wikiversion_changes = []
+    for change in all_changes_with_version:
+        wikiversion_changes.append(wikiversion_info(version, change))
+    return wikiversion_changes
 
 
 if __name__ == '__main__':
@@ -223,12 +293,20 @@ if __name__ == '__main__':
 
     patches_for_version = {}
     for version in args.versions:
-        patches, rollbacks, count = get_version_info(version)
-        patches_for_version[version] = patches
+        wikiversion_changes = get_wikiversion_changes(version)
+        if wikiversion_changes is None:
+            raise RuntimeError(f'{version} was never deployed!')
+
         if args.rollbacks_only:
-            print(rollbacks)
+            print(count_rollbacks(version, wikiversion_changes))
+            print(get_conductor(version, wikiversion_changes))
+            print(time_rolledback(version, wikiversion_changes))
+            print(train_delays(version, wikiversion_changes))
+            print(total_train_time(version, wikiversion_changes))
             sys.exit(0)
 
+        patches, count = get_patches_for_version(version)
+        patches_for_version[version] = patches
         with open(f'data/{version}.csv', 'w', newline='') as csvfile:
             fieldnames = [
                 'created',
