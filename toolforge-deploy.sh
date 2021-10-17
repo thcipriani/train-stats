40ded92d0db09877c7965fb68981127661643706
Tyler Cipriani <tcipriani@wikimedia.org>
2021-10-17 14:05:54 -0600

    Add quicky deployment script

---
+++ toolforge-deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Deploys latest main in webroot on toolforge automagically!
+#
+# USAGE ./toolforge-deploy.sh
+
+set -euo pipefail
+
+HOST=tools-login.wmflabs.org
+USER=tools.trainbow
+WEBROOT=/data/project/trainbow/www/python/src
+
+sshcmd() {
+    ssh "$HOST" -- sudo -u "$USER" "$@"
+}
+
+sshcmd git -C "$WEBROOT" remote update
+sshcmd git -C "$WEBROOT" checkout --force origin/main
+sshcmd git -C "$WEBROOT" status
+sshcmd git -C "$WEBROOT" -c gc.auto gc --auto --quiet
+sshcmd webservice --backend=kubernetes python3.7 restart
